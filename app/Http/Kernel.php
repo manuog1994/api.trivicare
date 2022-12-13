bdd82df451564024853f65b267e163295122f09c
Manuel <manuelortegagaliano@outlook.com>
2022-12-13 06:39:36 +0100

    update kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,8 +42,10 @@ class Kernel extends HttpKernel
 
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'throttle:60,1',
+            'bindings',
+            'auth:api',
         ],
         
         'sessions' => [
